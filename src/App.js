f5bff95921271d1a32b5a4d3912e818197c69e78
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-19 22:32:16 +0700

    updating the app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Swith, Route, Link } from "react-router-don";
+import { Layout, Typography, Space } from "antd";
 
 const App = () => {
-	return <div>Cryptocurrencies</div>;
+	return (
+		<div className="app">
+			<div className="navbar"></div>
+			<div className="main"></div>
+			<div className="footer"></div>
+		</div>
+	);
 };
 
 export default App;
